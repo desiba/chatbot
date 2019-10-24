19c485917e0ed137eaa4a2b15355c5e9d9ea18d9
desiba <desiba_1@yahoo.com>
2019-10-24 09:27:36 +0100

    modify env file

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,6 +1,6 @@
 var botui = new BotUI('api-bot');
 
-var socket = io.connect('https://aellachatbot.herokuapp.com/' + process.env.PORT || ':8010');
+var socket = io.connect('https://aellachatbot.herokuapp.com:8010');
 
 //var socket = io.connect(window.location.hostname + process.env.PORT || ':8010');
 //var socket = io.connect('http://localhost:8010');
