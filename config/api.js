7badc43c9b1163f1c33fc90a1b116f43ca26b1ec
desiba <desiba_1@yahoo.com>
2019-10-26 10:52:12 +0100

    modify env file

--- config/api.js
+++ config/api.js
@@ -1,14 +1,16 @@
 var apiai = require('apiai');
+var uuidv1 = require('uuid/v1');
 
 // read the api.ai docs : https://api.ai/docs/
 
 //Enter your API Key
 var app = apiai(process.env.API_AI_KEY);
 
+
 // Function which returns speech from api.ai
 var getRes = function(query) {
   var request = app.textRequest(query, {
-      sessionId: '<unique session id>'
+      sessionId: uuidv1()
   });
 const responseFromAPI = new Promise(
         function (resolve, reject) {
@@ -19,12 +21,19 @@ const responseFromAPI = new Promise(
   request.on('response', function(response) {
       resolve(response.result.fulfillment.speech);
   });
+
+  request.on('error', function(error) {
+    console.log(error);
+  });
+
 });
 request.end();
   return responseFromAPI;
 };
 
 // test the command :
-//getRes('hello').then(function(res){console.log(res)});
+getRes('hello').then(function(res){console.log(res)});
+getRes('why user is banned').then(function(res){console.log(res)});
+
 
 module.exports = {getRes}
