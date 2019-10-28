7286b52f685421a601ae419711e8017a9c97f52c
desiba <desiba_1@yahoo.com>
2019-10-28 11:57:17 +0100

    modify project

--- routes/index.js
+++ routes/index.js
@@ -66,6 +66,24 @@ return res.status(200).json({
       
 });
 
+
+// Webhook route
+router.post('/webhook', (req, res) => {
+  const data = req.body;
+  
+  console.log(data);
+
+	// Code the task you want to achieve with @data
+	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
+	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
+
+	const response = {
+		fulfillmentText: "Your webhook works fine !",
+	}
+	res.json(response);
+});
+
+
 router.post('/users', (req, res) => {
 
       console.log(req.body);
