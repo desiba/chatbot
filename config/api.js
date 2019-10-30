8e3766ed2f62161bd7c5fcb5285a0b8f6cffdfd2
desiba <desiba_1@yahoo.com>
2019-10-30 17:23:04 +0100

    modified project

--- config/api.js
+++ config/api.js
@@ -39,6 +39,7 @@ getRes('total loan disbursed').then(function(res){console.log(res)});
 getRes('total disbursment for today').then(function(res){console.log(res)});
 getRes('who linked this card 418742 9594').then(function(res){console.log(res)});
 getRes('why user was banned 24497').then(function(res){console.log(res)});
+getRes('why user was banned 4747489').then(function(res){console.log(res)});
 
 
 
