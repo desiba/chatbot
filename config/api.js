619f2190b4c812e05a83bcd004b59e3194a4de6a
desiba <desiba_1@yahoo.com>
2019-11-01 12:48:47 +0100

    code refactor in index page

--- config/api.js
+++ config/api.js
@@ -32,16 +32,16 @@ request.end();
 };
 
 // test the command :
-getRes('hi').then(function(res){console.log(res)});
-getRes('hello').then(function(res){console.log(res)});
-getRes('total number of users').then(function(res){console.log(res)});
-getRes('total loan disbursed').then(function(res){console.log(res)});
-getRes('total disbursment for today').then(function(res){console.log(res)});
-getRes('539941 9993 03/20 please help check who linked card').then(function(res){console.log(res)});
+//getRes('hi').then(function(res){console.log(res)});
+//getRes('hello').then(function(res){console.log(res)});
+//getRes('total number of users').then(function(res){console.log(res)});
+//getRes('total loan disbursed').then(function(res){console.log(res)});
+//getRes('total disbursment for today').then(function(res){console.log(res)});
+//getRes('539941 9993 03/20 please help check who linked card').then(function(res){console.log(res)});
 
-getRes('who linked this card 418742 9594 12/20').then(function(res){console.log(res)});
-getRes('why user was banned 24497').then(function(res){console.log(res)});
-getRes('why user was banned 4747489').then(function(res){console.log(res)});
+//getRes('who linked this card 418742 9594 12/20').then(function(res){console.log(res)});
+//getRes('why user was banned 24497').then(function(res){console.log(res)});
+//getRes('why user was banned 4747489').then(function(res){console.log(res)});
 
 
 
