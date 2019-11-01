2ebd9c25dd67da67de759e66d874334fdcc7d8d5
desiba <desiba_1@yahoo.com>
2019-11-01 23:56:18 +0100

    debugging

--- services/loan_services.js
+++ services/loan_services.js
@@ -4,10 +4,10 @@ const thousands = require('thousands');
 
 module.exports = {
 
-    total_loan_disbursed_range :  async function({start, end}, req, res){
+    total_loan_disbursed_range :   function({start, end}, req, res){
         
 
-        await dbConn.query(`SELECT SUM(amount) AS total_loan_disbursed FROM loan_requests WHERE approval_status IN (1,3,7,9) AND loan_starts BETWEEN '${start}' AND '${end}' `,  (error, data) => {
+         dbConn.query(`SELECT SUM(amount) AS total_loan_disbursed FROM loan_requests WHERE approval_status IN (1,3,7,9) AND loan_starts BETWEEN '${start}' AND '${end}' `,  (error, data) => {
             if (error) throw error;
   
                 console.log(data);
