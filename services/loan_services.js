633dbba22b8255b00a26719762e3f7db96b3b40c
desiba <desiba_1@yahoo.com>
2019-11-01 13:12:31 +0100

    moment error corrected

--- services/loan_services.js
+++ services/loan_services.js
@@ -10,9 +10,11 @@ module.exports = {
 
         let today =  now.format("YYYY-MM-DD");
 
+        console.log(today);
+
         dbConn.query(`SELECT SUM(amount) AS total_loan_disbursed FROM loan_requests WHERE approval_status IN (1,3,7,9) AND loan_starts = ${today}`,  (error, data) => {
           if (error) throw error;
-          
+
               console.log(data);
 
               let result_total_disbursment = data[0].total_loan_disbursed
