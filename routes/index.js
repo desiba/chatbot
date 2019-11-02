651322308bca0719b605d1c30d4b5a625c8c07f4
desiba <desiba_1@yahoo.com>
2019-11-02 12:44:39 +0100

    debugging features

--- routes/index.js
+++ routes/index.js
@@ -118,14 +118,20 @@ router.post('/webhook', async (req, res) => {
             
             let account_digits = parameters.card-digits;
             let card_dates = parameters.card-date;
+            var temp = new Array();
 
-            let account_digits_list = account_digits.split(" ");
+            temp = account_digits.split(" ");
+            account_digits_list = temp;
             let card_date_list = card_dates.split("/");
+           
 
             console.log(card_date_list.isArray());
+
+            // this will return an array with strings "1", "2", etc.
+            
             //console.log(Array.isArray(account_digits_list));
             //console.log(Array.isArray(card_date_list));
-
+            break;
 
             console.log(account_digits_list[0] +' '+ card_date_list[0]);
             
