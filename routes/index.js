86ecf035784f047b7d32f71d1573fabea3224850
desiba <desiba_1@yahoo.com>
2019-11-04 16:51:22 +0100

    debugging

--- routes/index.js
+++ routes/index.js
@@ -133,13 +133,13 @@ router.post('/webhook', async (req, res) => {
             let card_year_formatted = /\b[0-9]{4}/g.test(cardyear) ? cardyear : '20'+cardyear;
 
            console.log(last4digits +' '+ first6digits +' '+ cardmonth +' '+ card_year_formatted);
-           break
+           
            await dbConn.query(`SELECT email DISTINCT
                                FROM user_cards 
-                               WHERE last4 = ${last4digits} AND 
-                                      bin = ${first6digits} AND 
-                                      exp_month = ${cardmonth} AND 
-                                      exp_year = ${card_year_formatted} `,  (error, data) => {
+                               WHERE last4 = '${last4digits}' AND 
+                                      bin = '${first6digits}' AND 
+                                      exp_month = '${cardmonth}' AND 
+                                      exp_year = '${card_year_formatted}' `,  (error, data) => {
               console.log(data);
               if (error) throw error;
               if (!data.length){
