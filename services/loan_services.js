b15b13318b549eca71cfddb73d31d18456d3f030
desiba <desiba_1@yahoo.com>
2019-11-19 12:16:16 +0100

    remove  debug break

--- services/loan_services.js
+++ services/loan_services.js
@@ -17,7 +17,6 @@ module.exports = {
 
         console.log(loan_date);
 
-        break
         
         await db.sequelize.query(`SELECT SUM(amount) AS total_loan_by_date FROM loan_requests WHERE approval_status IN (1,3,7,9) AND loan_starts = '${loan_date}'`,  { type: sequelize.QueryTypes.SELECT})
         .then(function(data){
