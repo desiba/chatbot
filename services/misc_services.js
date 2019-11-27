5ae6e97d6947fff183a340153fa020dad6284ce0
desiba <desiba_1@yahoo.com>
2019-11-27 15:36:39 +0100

    code refactor

--- services/misc_services.js
+++ services/misc_services.js
@@ -8,7 +8,7 @@ module.exports = {
 
     payment_payload : async function(tag, req, res){
 
-        axios.get(`http://amoneyadminlivedashboard.eu-west-3.elasticbeanstalk.com/api/v1/analytics-summary?tag=${tag}`)
+        axios.get(`http://amoneyadminlivedashboard.eu-west-3.elasticbeanstalk.com/api/v1/analytics-summary?tag=this-year`)
             .then(function (response) {
 
              
