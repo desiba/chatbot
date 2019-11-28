0f210da375e243f08958e3a849ee6463d0bf3ad3
desiba <desiba_1@yahoo.com>
2019-11-28 17:08:44 +0100

    sql query

--- services/misc_services.js
+++ services/misc_services.js
@@ -27,14 +27,19 @@ module.exports = {
 
     auto_charge_user : async function(user_id, req, res){
 
+            console.log(user_id)
 
         await db.sequelize.query(`select l.id from loan_requests l left join users u on u.id = l.user_id where (email = '${user_id}' or u.id = '${user_id}') order by l.created_at desc limit 1`,  { type: sequelize.QueryTypes.SELECT})
         .then(function(data){
 
             console.log(data);
 
-           await axios.get(`https://adminreadonly.aellacredit.com/api/v1/loanrequests/charge-hundred-percent/loan/${data[0].id}`)
+            axios.get(`https://adminreadonly.aellacredit.com/api/v1/loanrequests/charge-hundred-percent/loan/${data[0].id}`)
             .then(function (response) {
+
+                console.log(response);
+
+                
 
                 let result_auto_charge = response.data.message
             
